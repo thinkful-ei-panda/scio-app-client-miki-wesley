659e33c3e7a6b0aa7b3c685b8c56bbbd2f18ca45
mischkiey <mischkieycf@gmail.com>
2020-08-31 18:45:10 -0400

    Automated env-dependent start scripts and added example.env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: (process.env.REACT_APP_ENV === 'production')
+    ? 'https://floating-ridge-78190.herokuapp.com/api'
+    : 'http://localhost:8000/api',
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'scio-app-auth-token',
 }
